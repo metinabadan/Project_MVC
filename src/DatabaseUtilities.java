4e2b88d1de34caf24abb72ee9ad3772b315cbe40
metinabadan <metinabadan@gmail.com>
2023-01-17 14:28:30 +0300

    elementary objects are created

--- src/DatabaseUtilities.java
+++ src/DatabaseUtilities.java
@@ -6,4 +6,26 @@ public class DatabaseUtilities
 	private static Connection connection = null;
 	public static String host;
 	public static String databaseName;
+	
+	public static Connection getConnection() throws SQLException 
+	{
+		if (connection == null) 
+		{
+			String conUrl =   "jdbc:sqlserver://" + host + ";"
+							+ "databaseName=" + databaseName + ";"
+							+ "integratedSecurity=true";
+
+			connection = DriverManager.getConnection(conUrl);
+		}
+		
+		return connection;
+	}
+	
+	public static void disconnect() throws SQLException
+	{
+		if (connection == null) 
+		{
+			connection.close();
+		}
+	}
 }
